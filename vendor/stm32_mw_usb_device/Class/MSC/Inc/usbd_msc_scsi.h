af617bb7eb7eaf6cf4ab34858393e291daa65f39
CLU <clu@zeroneng.com>
2026-06-05 17:04:27 -0700

    Reapply "Add MSC option to usb-comp composite sample"
    This reverts commit 0afcdf81718707f7b0be5b52f4053180b684a95a.

--- vendor/stm32_mw_usb_device/Class/MSC/Inc/usbd_msc_scsi.h
+++ vendor/stm32_mw_usb_device/Class/MSC/Inc/usbd_msc_scsi.h
@@ -60,6 +60,8 @@ extern "C" {
 
 #define SCSI_REQUEST_SENSE                          0x03U
 #define SCSI_START_STOP_UNIT                        0x1BU
+#define SCSI_SYNCHRONIZE_CACHE10                    0x35U
+#define SCSI_SYNCHRONIZE_CACHE16                    0x91U
 #define SCSI_TEST_UNIT_READY                        0x00U
 #define SCSI_WRITE6                                 0x0AU
 #define SCSI_WRITE10                                0x2AU
@@ -180,4 +182,3 @@ void SCSI_SenseCode(USBD_HandleTypeDef *pdev, uint8_t lun, uint8_t sKey,
 /**
   * @}
   */
-
